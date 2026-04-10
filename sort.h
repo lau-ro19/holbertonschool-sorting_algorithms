d8364929c00a97c6d26016d908e6e1f637da3531
lau-ro19 <lroseantoinette@gmail.com>
2026-04-10 12:21:32 +0200

    C - SortAlgs - Exo 02 - Selection Sort Algorithm
    COMPILER COMMAND
    gcc -Wall -Wextra -Werror -pedantic
      2-main.c 2-selection_sort.c print_array.c
      -o SortAlgs_Exo-02-select

---
+++ sort.h
@@ -0,0 +1,36 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>   /* Required for size_t type */
+
+/* === Externally provided resources === */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Print_Array - printing array items on a single line. */
+void print_array(const int *array, size_t size);
+
+/* Print_list - printing list items on a single line. */
+void print_list(const listint_t *list);
+
+/* SortAlgs - Exo 00 - 0-bubble_sort.c  */
+void bubble_sort(int *array, size_t size);
+/* SortAlgs - Exo 01 - 1-insertion_sort_list.c  */
+void insertion_sort_list(listint_t **list);
+/* SortAlgs - Exo 02 - 2-selection_sort.c  */
+void selection_sort(int *array, size_t size);
+/* SortAlgs - Exo 03 - 3-quick_sort.c  */
+
+#endif
